1f32d7db997148caeb1bfcb52c009feecb801772
Zadock <malobazadock@gmail.com>
2021-05-20 15:16:53 +0300

    Boot: Created printstring function.

--- bootloader/bootloader.asm
+++ bootloader/bootloader.asm
@@ -1,9 +1,28 @@
+
+[org 0x7c00]
  
-mov ah, 0x0e
-mov al, 'N'
-int 0x10
+mov bp, 0x7c00
+mov sp, bp
+
+mov bx, NaiSysString
+call printstring
 
 jmp $
+
+printstring:
+    mov ah, 0x0e
+    .Loop:
+    cmp [bx], byte 0
+    je .Exit
+        mov al, [bx]
+        int 0x10
+        inc bx
+        jmp .Loop
+    .Exit:
+    ret
+
+NaiSysString:
+    db 'NaiSys Operating System',0
 
 times 510-($-$$) db 0
 
